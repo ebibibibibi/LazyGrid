fb4da89fc5be4ee6366b41cd0707d5627c275c3c
ebibibibibi <xvipea0@gmail.com>
2022-08-25 18:29:20 +0700

    Initial Commit

---
+++ LazyGrid/LazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridApp.swift
+//  LazyGrid
+//
+//  Created by kotomi tahkahashi on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
