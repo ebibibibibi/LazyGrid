6953598d3a7582e1bdb14c3f48bf8fe4821f10a5
ebibibibibi <xvipea0@gmail.com>
2022-08-27 17:55:55 +0700

    GridViewのサンプル。

--- LazyGrid/ContentView.swift
+++ LazyGrid/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let contentWidth = UIScreen.main.bounds.size.width * 1 / 4
+    let columns = [GridItem(.fixed(80), spacing: 18)]
+    let columnss = [GridItem(.flexible(minimum: 80, maximum: 85), spacing: 18)]
+    let columnsss = [GridItem(.adaptive(minimum: 80), spacing: 18)]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView(.vertical) {
+                /// ここのspacingは行間(縦)を指定している。
+                LazyVGrid(columns: columnsss, spacing: 30, pinnedViews: .sectionHeaders) {
+                    Section(header: Text("**ヘッダーだよ**")) {
+                        ForEach(0...2, id: \.self) { data in
+                            VStack {
+                                Rectangle()
+                                    .frame(width: contentWidth, height: contentWidth * 0.57)
+                                    .foregroundColor(Color("liteBlue"))
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
